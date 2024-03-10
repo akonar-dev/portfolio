bc8440123f13047f4ed734b20c4b99e909fffd1b
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2024-03-10 18:04:26 +0530

    Update main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <ul className="flex flex-1 justify-center space-x-10 bg-slate-400 p-7">
             <li>About</li>
             <li>Work Experience</li>
-            <li>Academics</li>
+            <li>Academics and Sports </li>
             <li>Projects</li>
             <li>Hobbies</li>
             <li>Achievements</li>
